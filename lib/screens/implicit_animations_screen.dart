bee726a98a95c9b75813ae3754b3919ed78ae6d0
yoonpapa <47910645+ssaulabihoon@users.noreply.github.com>
2023-12-11 15:23:51 +0900

    Implicit animations

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -8,11 +8,48 @@ class ImplicitAnimationsScreen extends StatefulWidget {
 }
 
 class _ImplicitAnimationsScreen extends State<ImplicitAnimationsScreen> {
+  bool _visible = true;
+  void _trigger() {
+    setState(() {
+      _visible = !_visible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Implict Animations"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TweenAnimationBuilder(
+              tween: ColorTween(
+                begin: Colors.yellow,
+                end: Colors.red,
+              ),
+              curve: Curves.bounceInOut,
+              duration: const Duration(seconds: 5),
+              builder: (context, value, child) {
+                return Image.network(
+                  "https://upload.wikimedia.org/wikipedia/commons/4/4f/Dash%2C_the_mascot_of_the_Dart_programming_language.png",
+                  color: value,
+                  colorBlendMode: BlendMode.colorBurn,
+                );
+              },
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              onPressed: _trigger,
+              child: const Text("Go!"),
+            ),
+          ],
+        ),
       ),
     );
   }
