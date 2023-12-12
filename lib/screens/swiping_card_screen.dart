57cc04ce846750458a5d7f98aaa2b1168333e393
yoonpapa <47910645+ssaulabihoon@users.noreply.github.com>
2023-12-12 14:42:25 +0900

    Swiping cards project

--- lib/screens/swiping_card_screen.dart
+++ lib/screens/swiping_card_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SwipingCardsScreen extends StatefulWidget {
@@ -7,13 +10,233 @@ class SwipingCardsScreen extends StatefulWidget {
   State<SwipingCardsScreen> createState() => _SwipingCardsScreenState();
 }
 
-class _SwipingCardsScreenState extends State<SwipingCardsScreen> {
+class _SwipingCardsScreenState extends State<SwipingCardsScreen>
+    with SingleTickerProviderStateMixin {
+  late final size = MediaQuery.of(context).size;
+
+  late final AnimationController _position = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 1),
+    lowerBound: (size.width + 100) * -1,
+    upperBound: (size.width + 100),
+    value: 0.0,
+  );
+
+  late final Tween<double> _rotation = Tween(
+    begin: -15,
+    end: 15,
+  );
+
+  late final Tween<double> _scale = Tween(
+    begin: 0.8,
+    end: 1,
+  );
+
+  late final ColorTween _closeColor = ColorTween(
+    begin: Colors.white,
+    end: Colors.red,
+  );
+
+  late final ColorTween _checkColor = ColorTween(
+    begin: Colors.white,
+    end: Colors.green,
+  );
+  late final Tween<double> _buttonScale = Tween(
+    begin: 1.0,
+    end: 1.2,
+  );
+
+  void _onHorizontalDragUpdate(DragUpdateDetails details) {
+    _position.value += details.delta.dx;
+  }
+
+  void _wenComplete() {
+    _position.value = 0;
+    setState(() {
+      _index = _index == 5 ? 1 : _index + 1;
+    });
+  }
+
+  void _onHorizontalDragEnd(DragEndDetails details) {
+    final bound = size.width - 200;
+    final dropzone = size.width + 100;
+    if (_position.value.abs() >= bound) {
+      final factor = _position.value.isNegative ? -1 : 1;
+      _position
+          .animateTo(
+            (dropzone) * factor,
+            curve: Curves.easeOut,
+          )
+          .whenComplete(_wenComplete);
+    } else {
+      _position.animateTo(
+        0,
+        curve: Curves.easeOut,
+      );
+    }
+  }
+
+  void _xButtonPressed() {
+    _position.reverse().whenComplete(() {
+      _wenComplete();
+    });
+  }
+
+  void _cButtonPressed() {
+    _position.forward().whenComplete(() {
+      _wenComplete();
+    });
+  }
+
+  @override
+  void dispose() {
+    _position.dispose();
+    super.dispose();
+  }
+
+  int _index = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Swiping Cards"),
       ),
+      body: AnimatedBuilder(
+        animation: _position,
+        builder: (context, child) {
+          final angle = _rotation.transform(
+                (_position.value + size.width / 2) / size.width,
+              ) *
+              pi /
+              180;
+          final scale = _scale.transform(_position.value.abs() / size.width);
+          final buttonScale =
+              _buttonScale.transform(_position.value.abs() / size.width);
+          final xColor =
+              _closeColor.transform(_position.value.abs() / size.width);
+          final cColor =
+              _checkColor.transform(_position.value.abs() / size.width);
+          return Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Positioned(
+                top: 100,
+                child: Transform.scale(
+                  scale: min(scale, 1.0),
+                  child: Card(
+                    index: _index == 5 ? 1 : _index + 1,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 100,
+                child: GestureDetector(
+                  onHorizontalDragUpdate: _onHorizontalDragUpdate,
+                  onHorizontalDragEnd: _onHorizontalDragEnd,
+                  child: Transform.translate(
+                    offset: Offset(_position.value, 0),
+                    child: Transform.rotate(
+                      angle: angle,
+                      child: Card(
+                        index: _index,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 670,
+                child: Transform.scale(
+                  scale: min(buttonScale, 1.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: _xButtonPressed,
+                        child: CircleButton(
+                          icon: const Icon(
+                            Icons.close,
+                            //color: Colors.white,
+                          ),
+                          color: xColor,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 50,
+                      ),
+                      GestureDetector(
+                        onTap: _cButtonPressed,
+                        child: CircleButton(
+                          icon: const Icon(
+                            Icons.check,
+                            //color: Colors.white,
+                          ),
+                          color: cColor,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  final int index;
+
+  const Card({
+    super.key,
+    required this.index,
+  });
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Material(
+      elevation: 10,
+      borderRadius: BorderRadius.circular(10),
+      clipBehavior: Clip.hardEdge,
+      child: SizedBox(
+        width: size.width * 0.8,
+        height: size.height * 0.6,
+        child: Image.asset(
+          "assets/covers/$index.jpg",
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class CircleButton extends StatelessWidget {
+  final Color? color;
+  final Icon icon;
+
+  const CircleButton({
+    super.key,
+    required this.color,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        border: Border.all(
+          color: Colors.grey.shade300,
+          width: 3,
+        ),
+        color: color,
+      ),
+      child: icon,
     );
   }
 }
